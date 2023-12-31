0e64c83e5c0eb3d42a44fde56a416dbf637696e7
HakimiDev <hakimideveloper@gmail.com>
2023-12-31 21:51:53 +0300

    Add auth with password

--- index.js
+++ index.js
@@ -1,70 +1,82 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express';
-import cors from 'cors';
-import { readFileSync, writeFileSync } from 'node:fs';
+import express from "express";
+import cors from "cors";
+import { readFileSync, writeFileSync } from "node:fs";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('json spaces', 2);
-app.use(cors({
-    origin: '*'
-}));
+app.set("json spaces", 2);
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use((req, res, next) => {
+  const pass = req.headers["authorization"];
+  if (pass !== process.env.PASS) {
+    return res.sendStatus(401); // Unauthorized
+  }
+  next();
+});
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const data = readFileSync('data.json');
-    return res.status(200).json(JSON.parse(data));
+router.get("/", async (req, res) => {
+  const data = readFileSync("data.json");
+  return res.status(200).json(JSON.parse(data));
 });
 
-router.post('/', async (req, res) => {
-    const { token } = req.body;
-    if (typeof token !== 'string' || !token.trim().length) {
-        return res.sendStatus(404);
-    }
+router.post("/", async (req, res) => {
+  const { token } = req.body;
+  if (typeof token !== "string" || !token.trim().length) {
+    return res.sendStatus(404);
+  }
 
-    const data = JSON.parse(readFileSync('data.json'));
-    const tokens = data.tokens;
-    if (tokens.find(t => t === token)) {
-        return res.sendStatus(409);
-    }
+  const data = JSON.parse(readFileSync("data.json"));
+  const tokens = data.tokens;
+  if (tokens.find((t) => t === token)) {
+    return res.sendStatus(409);
+  }
 
-    tokens.push(token);
-    writeFileSync('data.json', JSON.stringify(data, 2, 2, 2));
-    return res.status(200).json(data);
+  tokens.push(token);
+  writeFileSync("data.json", JSON.stringify(data, 2, 2, 2));
+  return res.status(200).json(data);
 });
 
-router.patch('/', async (req, res) => {
-    const { token } = req.body;
-    if (typeof token !== 'string' || !token.trim().length) {
-        return res.sendStatus(404);
-    }
+router.patch("/", async (req, res) => {
+  const { token } = req.body;
+  if (typeof token !== "string" || !token.trim().length) {
+    return res.sendStatus(404);
+  }
 
-    const data = JSON.parse(readFileSync('data.json'));
-    let tokens = data.tokens;
-    if (!tokens.find(t => t === token)) {
-        return res.sendStatus(404);
-    }
+  const data = JSON.parse(readFileSync("data.json"));
+  let tokens = data.tokens;
+  if (!tokens.find((t) => t === token)) {
+    return res.sendStatus(404);
+  }
 
-    tokens = tokens.filter(t => t !== token);
-    data.tokens = tokens;
-    writeFileSync('data.json', JSON.stringify(data, 2, 2, 2));
-    return res.status(200).json(data);
+  tokens = tokens.filter((t) => t !== token);
+  data.tokens = tokens;
+  writeFileSync("data.json", JSON.stringify(data, 2, 2, 2));
+  return res.status(200).json(data);
 });
 
-router.get('/check/:token', async (req, res) => {
-    const { token } = req.params;
-    const data = JSON.parse(readFileSync('data.json'));
-    if (data.tokens.find(t => t === token)) return res.sendStatus(200);
-    return res.sendStatus(400);
+router.get("/check/:token", async (req, res) => {
+  const { token } = req.params;
+  const data = JSON.parse(readFileSync("data.json"));
+  if (data.tokens.find((t) => t === token)) return res.sendStatus(200);
+  return res.sendStatus(400);
 });
 
-app.use('/tokens', router);
+app.use("/tokens", router);
 
-app.all('*', (req, res) => res.sendStatus(404));
+app.all("*", (req, res) => res.sendStatus(404));
 
-const server = app.listen(process.env.PORT, () => console.log("Server is online"));
+const server = app.listen(process.env.PORT, () =>
+  console.log("Server is online")
+);
